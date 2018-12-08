20335e6237d6c6f2835b3b644e8ac22e7fcd2177
Martins Mozeiko <martins.mozeiko@gmail.com>
2018-12-08 13:07:02 -0800

    i3blocks config

---
+++ .local/bin/btc.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+response=$(curl -L -s -w "\n%{http_code}\n" "https://www.bitstamp.net/api/v2/ticker/btcusd/")
+code=$(echo "$response" | sed -n '$p')
+res=$(echo "$response" | sed '$d')
+
+case "$code" in
+  200) echo "$res" | jq -r ".last" | awk '{printf "%.2f", $1}'
+       ;;
+    *) echo -e "??"
+       exit 1
+       ;;
+esac
